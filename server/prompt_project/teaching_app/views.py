33c4987cf1a2fbade083dda0228dee4897fea081
Aayush Thapaliya <106351327+aayush6200@users.noreply.github.com>
2023-07-01 00:42:42 -0700

    db added for ml

--- server/prompt_project/teaching_app/views.py
+++ server/prompt_project/teaching_app/views.py
@@ -20,46 +20,133 @@ json_file_path_l3 = "JSON/l3.json"
 json_file_path_l4 = "JSON/l4.json"
 
 with open(json_file_path_l3) as file:
-    l3_data = json.load(file)
+    positions = json.load(file)
 
 
-# system_message = f''' "role":'system', "content":'As an assistant, your role is to provide information to user from {l3_data} by listening user queries'''
+prompt = f"""
+- If the user wants to provide their qualifications, analyze their qualifications and recommend the best position from the {positions}. If the user is uncertain about what they are looking for, respond kindly and ask for clarification.
+
+Please follow the above steps to assist users with their queries.
+"""
 
 
+# saves the user information
 def save_response(email, text, res):
     try:
-        chat_message = ChatMessage(
-            email=email,
-            user_message=text,
-            chatgpt_response=res
-        )
 
-        chat_message.save()
+        # saving user and gpt response for analyzing results for future queries
+
+        if (len(text) > 0 and len(res) == 0):
+            chat_message = ChatMessage(
+                email=email,
+                user_message=text,
+
+            )
+            chat_message.save()
+        elif (len(res) == 0):
+            chat_message = ChatMessage(
+                email=email,
+                chatgpt_response=res
+            )
+
+            chat_message.save()
         print('saved')
         return
     except ValueError as e:
         raise e
 
-    print('saved')
+    print('not saved')
+
+# getting response from gpt model using db.This will be used for analysis purpose
 
 
-# generating response from gpt
+def gpt_analysis(user_email, text, model='gpt-3.5-turbo'):
 
+    # Retrieve the latest conversations involving the specific user
+    latest_conversations = ChatMessage.objects.filter(
+        email=user_email).order_by('-created_at')[:10]
 
-def get_gpt_response(email, message, text, model='gpt-3.5-turbo'):
+    # Concatenate user's messages to form conversation history
+    conversation_history = ''
+
+    for conv in latest_conversations:
+        conversation_history += "User: " + conv.user_message + "\n"
+        conversation_history += "GPT: " + conv.chatgpt_response + "\n"
+
+        # Prepare instructions or prompts for GPT
+        instructions = f"You are analyzing the conversation with the user.\
+            Generate insights based on the following conversation history and respond politely\
+                :Use {prompt} for further insights\
+                Only provide answer don't provide answer like:based on user response  \n\n"
+        input_prompt = instructions + '\n' + conversation_history
+
+    # Generate reply from GPT
     try:
         response = openai.ChatCompletion.create(
             model=model,
-            messages=message,
+            messages=[{'role': 'system', 'content': input_prompt},
+                      {'role': 'user', 'content': text}],
             temperature=0
         )
 
+        # only saving results from gpt
+        save_response(
+            user_email, '', response.choices[0].message['content'])
+        return response.choices[0].message['content']
+    except Exception as e:
+        print('internal error', e)
+
+# gets list of all the data for analysis
+
+
+# def get_data_for_analysis(email):
+#     print('get messages', email)
+
+#     if email:
+#         try:
+#             chat_messages = ChatMessage.objects.filter(email=email)
+#             # creating a list of dictionaries for each message and responses
+
+#             chats = [{'user_chat': msg.user_message,
+#                       'gpt_chat': msg.chatgpt_response, 'id': msg.id} for msg in chat_messages]
+
+#             # returning successful messages and chats for analysis
+#             response_message = 'data for query received'
+#             print('at data analysis', chats)
+#             return ([chats, response_message])
+
+#         except Exception as e:
+#             print('error', e)
+
+#     # returning error messages
+#     response_message = 'error for query/may be internal error'
+#     return [response_message]
+
+
+# generating response from gpt
+def get_gpt_response(email, text, model='gpt-3.5-turbo'):
+    try:
+        print('hello wo')
+        # response = openai.ChatCompletion.create(
+        #     model=model,
+        #     messages=message,
+        #     temperature=0.8
+        # )
+
         # saving response calling the function in database
         try:
-            save_response(
-                email, text, response.choices[0].message['content'])
 
-            return response.choices[0].message['content']
+            # saving query from user for future use
+            save_response(
+                email, text, '')
+
+            # # calling analysis function for getting data to analyze using gpt
+            # data_to_analyze = get_data_for_analysis(email)
+            # if len(data_to_analyze) > 1:
+            # calling gpt to analyze data from the previous conversations
+            after_gpt_response = gpt_analysis(email, text)
+            return after_gpt_response
+
         except ValueError as e:
             raise e
     except openai.OpenAIError as e:
@@ -83,16 +170,11 @@ def generate_response(request):
             text = text.get('userChat')
             print('text:', text)
             if text:
-                message = [
-                    {'role': 'system', 'content': f'You are an assistant who responds in \
-                     2 sentences max using {l3_data}'},
-                    {'role': 'user', 'content': f'{text}'}
-                ]
-                # prompt = "please provide at max 2 sentence help to user"
 
                 try:
+                    # calling gpt model from another function
                     response = get_gpt_response(
-                        email, message, text)
+                        email,  text)
 
                     response_data = {
                         'message': 'gpt replied successfully',
@@ -121,24 +203,25 @@ def generate_response(request):
         return JsonResponse(response_data, status=500)
 
 
-def get_response(request):
-    if request.method == 'GET':
-        email = request.email
-        print('get messages', email)
+# getting response from gpt model using db.
+# def get_response(request):
+#     if request.method == 'GET':
+#         email = request.email
+#         print('get messages', email)
 
-        if email:
-            try:
-                chat_messages = ChatMessage.objects.filter(email=email)
-                # creating a list of dictionaries for each message and responses
+#         if email:
+#             try:
+#                 chat_messages = ChatMessage.objects.filter(email=email)
+#                 # creating a list of dictionaries for each message and responses
 
-                chats = [{'user_chat': msg.user_message,
-                          'gpt_chat': msg.chatgpt_response, 'id': msg.id} for msg in chat_messages]
-                return JsonResponse(chats, status=200, safe=False)
-            except Exception as e:
-                print('error', e)
-                return JsonResponse({'message': 'internal server error'}, status=500)
-        else:
-            return JsonResponse({'message': 'error occurred'}, status=500)
+#                 chats = [{'user_chat': msg.user_message,
+#                           'gpt_chat': msg.chatgpt_response, 'id': msg.id} for msg in chat_messages]
+#                 return JsonResponse(chats, status=200, safe=False)
+#             except Exception as e:
+#                 print('error', e)
+#                 return JsonResponse({'message': 'internal server error'}, status=500)
+#         else:
+#             return JsonResponse({'message': 'error occurred'}, status=500)
 
 
 @csrf_exempt
